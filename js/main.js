c8f8f557282c65bb6d06531b5327fe13722dd20f
dehn hunsworth <dhunsworth@yahoo.com>
2019-02-11 00:16:29 -0700

    1st run at condensing menus via JS

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $intro.on("mouseover", function() {
   $text.css("textShadow", "2px 2px #333");
 });
 
-//Controls tabs
+//Controls tabs local weather
 function openCity(cityName) {
   var i;
   var x = document.getElementsByClassName("city");
@@ -18,3 +18,20 @@ function openCity(cityName) {
   }
   document.getElementById(cityName).style.display = "block";
 }
+
+//controls for global tabs
+const li = document.querySelectorAll(".global-menu li");
+const global = document.querySelectorAll("#global");
+
+function globalMenu(event) {
+  global.style.display = none;
+  if (event.target.innerText == "Local Weather") {
+    document.querySelector("#weather").style.display = "block";
+  } else if (event.target.innerText == "Snow Conditions") {
+    document.querySelector("#snow").style.display = "block";
+  } else if (event.target.innerText == "River Flows") {
+    document.querySelector("#river").style.display = "block";
+  }
+}
+
+li.addEventListener("click", globalMenu);
