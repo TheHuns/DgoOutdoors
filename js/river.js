d139b4562caeec7e059ed2de9120524a80889a3c
dehn hunsworth <dhunsworth@yahoo.com>
2019-02-12 12:24:17 -0700

    second commit working toward api calls

--- js/river.js
+++ js/river.js
@@ -1,12 +1,41 @@
-const http = require("http");
+// const http = require("http");
 const https = require("https");
 
-//print response function
+//print response function(to go in response.end part of the call)
+function printResults(streamInfo) {
+  // create html element and populate
+}
 
 //print error function
-
+function printError(error) {
+  console.error(error);
+}
 //api call
-function getFlows() {}
+function getFlows(location) {
+  try {
+    const request = https.get(
+      `https://streamstats.usgs.gov/streamstatsservices/flowstatistics.json?rcode=${location}`,
+      function(response) {
+        if (response.statusCode === 200) {
+          let body = "";
+          response.on("data", data => {
+            body += data;
+          });
+          response.on("end", () => {
+            let streamInfo = JSON.parse(body);
+            console.dir(streamInfo[2]);
+          });
+        } else {
+          console.log(response.statusCode);
+        }
+      }
+    );
+  } catch (error) {
+    printError(error.message);
+  }
+}
 
 //export module
-module.exports.getFlows = getFlows;
+// module.exports.getFlows = getFlows;
+
+getFlows("CO");
